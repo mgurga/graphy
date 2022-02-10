5f7bb03419fb022fc78ec18e60d1c7722f5bca4c
mgurga <motiurga@gmail.com>
2022-02-10 06:53:48 -0800

    moved renamenx to rename file

--- src/commands/rename.cpp
+++ src/commands/rename.cpp
@@ -13,3 +13,18 @@ string Graphy::rename(string s, Database* db)
     db->set(args.at(1), val);
     return "OK";
 }
+
+string Graphy::renamenx(string s, Database* db)
+{
+    Parser p;
+    vector<string> args = p.parse(s);
+    if (args.size() != 2) return "ERR incorrect number of arguments";
+    if (!db->key_exists(args.at(0))) return "ERR no such key";
+    if (db->key_exists(args.at(1))) return "(integer) 0";
+    string val = db->get(args.at(0));
+    db->delete_key(args.at(0));
+    if (db->key_exists(args.at(1)))
+        db->delete_key(args.at(1));
+    db->set(args.at(1), val);
+    return "(integer) 1";
+}
